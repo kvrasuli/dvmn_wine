225b46005fd0982fe35f5e64f17544996bba7b86
Kirill Rasuli <rasulikv@gmail.com>
2020-07-23 18:32:11 +0000

    First attempt to pass

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import xlrd
+import pprint
+from collections import defaultdict
+
+LIST_OF_WINES_NAME = 'wine3.xlsx'
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -10,18 +14,28 @@ env = Environment(
 )
 
 template = env.get_template('template.html')
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1', encoding='cp1251-sig')
-wines = excel_data_df.to_json(orient='records')
-print(wines)
+excel_data_df = pandas.read_excel(LIST_OF_WINES_NAME)
+wines_json_string = excel_data_df.to_json(orient='records')
+wines = pandas.read_json(wines_json_string).to_dict(orient='records')
+
+wines_by_category = defaultdict(list)
+for wine in wines:
+  wines_by_category[wine['Категория']].append(wine)
+
+printer = pprint.PrettyPrinter()
+printer.pprint(wines_by_category)
+
+categories = list(wines_by_category.keys())
 year = datetime.datetime.now().year
+age = str(year - 1920)
 
 rendered_page = template.render(
-    age=str(year - 1920),
+    age=age,
     wines=wines,
+    categories=categories,
 )
 
-
-
+pp = pprint.PrettyPrinter(indent=4)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
