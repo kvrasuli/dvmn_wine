dfbd657a3142e6fdacc36955c3d7ae062f0f42f2
Kirill Rasuli <rasulikv@gmail.com>
2020-07-27 12:25:02 +0300

    Second attempt. Fixes in template, readme, main.py

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 import xlrd
-import pprint
 from collections import defaultdict
+import argparse
+from dotenv import load_dotenv
+import os
 
-LIST_OF_WINES_NAME = 'wine3.xlsx'
+parser = argparse.ArgumentParser(description='Elite wines')
+parser.add_argument('--txp', help='Your path to test xls file')
+test_xls_path = parser.parse_args().txp
+
+if not test_xls_path:
+    load_dotenv()
+    test_xls_path = os.getenv('LIST_OF_WINES_NAME')
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -14,7 +22,7 @@ env = Environment(
 )
 
 template = env.get_template('template.html')
-excel_data_df = pandas.read_excel(LIST_OF_WINES_NAME)
+excel_data_df = pandas.read_excel(test_xls_path)
 wines_json_string = excel_data_df.to_json(orient='records')
 wines = pandas.read_json(wines_json_string).to_dict(orient='records')
 
@@ -22,23 +30,16 @@ wines_by_category = defaultdict(list)
 for wine in wines:
   wines_by_category[wine['Категория']].append(wine)
 
-printer = pprint.PrettyPrinter()
-printer.pprint(wines_by_category)
-
-categories = list(wines_by_category.keys())
 year = datetime.datetime.now().year
 age = str(year - 1920)
 
 rendered_page = template.render(
     age=age,
-    wines=wines,
-    categories=categories,
+    wines=wines_by_category,
 )
-
-pp = pprint.PrettyPrinter(indent=4)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+server = HTTPServer(('127.0.0.1', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
